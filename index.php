d1d20cf70e3072e12b6b5bc630517a4c671b9b06
SlimVl <slimvlasov@gmail.com>
2022-10-18 22:46:30 +0500

    q

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
+phpinfo();
 
+require_once __DIR__ . "/config/boot.php";
+include __DIR__ . "/header.php";
+
+include __DIR__ . "/footer.php";
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '41c27c303404f7b115c26a9ed228fc27cfb0cd15',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '41c27c303404f7b115c26a9ed228fc27cfb0cd15',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap-icons' => array(
+            'pretty_version' => 'v1.9.1',
+            'version' => '1.9.1.0',
+            'reference' => 'f7f6f61194419dcf4447dbe05b78a72601646490',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap-icons',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
